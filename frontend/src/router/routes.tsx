eae0dc006a3dd5928c88afafb65f8670108d12e5
Jilian Garette Abadia Abangan <jiliangarette@gmail.com>
2025-02-09 12:31:02 -0800

    fix routes configuration and added spinner as fallback

--- frontend/src/router/routes.tsx
+++ frontend/src/router/routes.tsx
@@ -1,16 +1,19 @@
 import { RouteObject } from "react-router-dom";
 import { Suspense } from "react";
 import HomePage from "../pages/HomePage";
+import TestPage from "../test/TestPage";
+import StandardEmail from "../pages/StandardEmail";
+import { LoadingSpinner } from "../components/ui/Spinner";
 
-const routes: RouteObject[] = [
-  {
-    path: "/",
-    element: (
-      <Suspense fallback={<div>Loading...</div>}>
-        <HomePage />
-      </Suspense>
-    ),
-  },
+const pages = [
+  { path: "/", component: <HomePage /> },
+  { path: "/test", component: <TestPage /> },
+  { path: "/standard", component: <StandardEmail /> },
 ];
+
+const routes: RouteObject[] = pages.map(({ path, component }) => ({
+  path,
+  element: <Suspense fallback={<LoadingSpinner />}>{component}</Suspense>,
+}));
 
 export default routes;
