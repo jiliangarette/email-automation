31f6e872d7be195c01e25dafee25d4b36b0d787b
Jilian Garette Abadia Abangan <jiliangarette@gmail.com>
2025-02-11 01:12:03 -0800

    optimize response time

--- backend/controllers/jobApplicationController.js
+++ backend/controllers/jobApplicationController.js
@@ -3,19 +3,13 @@ import { sendEmail } from "../services/emailService.js";
 export const standardEmailController = async (req, res) => {
   const { email, applicantName, jobPosition, hiringManager, resumeUrl } =
     req.body;
-  try {
-    await sendEmail({
-      email,
-      applicantName,
-      jobPosition,
-      hiringManager,
-      resumeUrl,
+  sendEmail({ email, applicantName, jobPosition, hiringManager, resumeUrl })
+    .then(() => {
+      console.log("Email sent successfully");
+    })
+    .catch((error) => {
+      console.error("Error sending email:", error);
     });
-    res
-      .status(200)
-      .send({ message: "Email sent successfully from standard template!" });
-  } catch (error) {
-    console.error("Error sending email:", error);
-    res.status(500).send({ message: "Failed to send email" });
-  }
+
+  res.status(200).send({ message: "Data received. Email is being sent!" });
 };
